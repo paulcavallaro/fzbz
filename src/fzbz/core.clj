17895707df828747a7c8206e86112b07a71adedd
Paul Cavallaro <paulcavallaro@gmail.com>
2011-03-31 03:32:35 -0400

    Woohoo lazy sequences

--- src/fzbz/core.clj
+++ src/fzbz/core.clj
@@ -12,6 +12,20 @@
                :else n))
        (take x naturals)))
 
+(def fzbz-seq
+  (map (fn [n]
+         (cond (== 0 (mod n 15)) "FizzBuzz"
+               (== 0 (mod n 5)) "Buzz"
+               (== 0 (mod n 3)) "Fizz"
+               :else n))
+       naturals))
+
 (defn print-fzbz [x]
   :doc "Prints the darn thing."
   (println (reduce (fn [h rst] (str h "\n" rst)) (fzbz x))))
+
+(defn print-fzbz2 [x]
+  :doc "Using lazy sequences this time"
+  (println (reduce
+            (fn [h rst] (str h "\n" rst))
+            (take x fzbz-seq))))
